a9b8d2e6b4e9e5c4671308b961ddacaf3215d937
Jose Luis Barbosa <kirbi0w0@gmail.com>
2026-03-23 20:42:05 -0600

    add tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,36 @@
 <template>
-    <div>
-        <section><span>Search...</span><Icon icon="material-symbols-light:search" width="28" color="#C2C2C2"/></section>
-        <P>QUE ESTAS PENSANDO?</P>
+  <div>
+    <section>
+      <span>Search...</span><Icon icon="heroicons:magnifying-glass" width="28" color="#C2C2C2" />
+    </section>
+    <P>QUE ESTAS PENSANDO?</P>
 
-        <!-- Posts Section -->
-        <section>
-            <article>
-                <header>
-                    <div>
-                        <img src="../assets/@lilithBlackheim.jfif" alt="@lilithBlackheim" title="@lilithBlackheim" width="50"/>
-                        <p>@lilithBlackheim</p>
-                    </div>
-                    <p>hace 1 hora</p>
-                </header>
-                <p>Los mas enamorados del mundo mundial 👩‍❤️‍👨</p>
-                <footer>
-                    <button>like</button>
-                    <button>comment</button>
-                    <button>share</button>
-                </footer>
-            </article>
-        </section>
-    </div>
+    <!-- Posts Section -->
+    <section>
+      <article>
+        <header>
+          <div>
+            <img
+              src="../assets/@lilithBlackheim.jfif"
+              alt="@lilithBlackheim"
+              title="@lilithBlackheim"
+              width="50"
+            />
+            <p>@lilithBlackheim</p>
+          </div>
+          <p>hace 1 hora</p>
+        </header>
+        <p>Los mas enamorados del mundo mundial 👩‍❤️‍👨</p>
+        <footer>
+          <button>like</button>
+          <button>comment</button>
+          <button>share</button>
+        </footer>
+      </article>
+    </section>
+  </div>
 </template>
-<script lang="ts" setup >
-import { Icon } from "@iconify/vue";
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
