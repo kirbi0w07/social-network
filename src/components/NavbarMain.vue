03183c22419e00d0c33c0f3fd94044de83642dc8
jbarbosac <jose.barbosa@infotec.mx>
2026-03-26 00:58:21 -0600

    navbar changes

--- src/components/NavbarMain.vue
+++ src/components/NavbarMain.vue
@@ -1,14 +1,23 @@
 <template lang="">
-    <nav>
-       <router-link to="/">Home</router-link> 
-       <router-link to="/profile">Profile</router-link> 
-       <router-link to="/messages">Messages</router-link> 
-       <router-link to="/search">Search</router-link> 
+    <nav class="flex gap-2 justify-around fixed bottom-0 bg-white py-2 w-full">
+        <router-link to="/">
+            <Icon icon="heroicons:home-16-solid" width="24" color="#374151"/>
+        </router-link> 
+        <router-link to="/profile">
+            <Icon icon="heroicons:user-circle-16-solid" width="24" color="#374151"/>
+        </router-link> 
+        <router-link to="/messages">
+            <Icon icon="heroicons:chat-bubble-oval-left-ellipsis-solid" width="24" color="#374151" />
+        </router-link> 
+        <router-link to="/search">
+    <Icon icon="heroicons:magnifying-glass-16-solid" width="24" color="#374151" class="-scale-x-100"/>
+        </router-link> 
     </nav>
 </template>
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
 
 </script>
 <style lang="">
-    
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import router from './router'
 
 const app = createApp(App)
 
+if (import.meta.env.DEV) {
+    app.config.globalProperties.__VUE_DEVTOOLS_GLOBAL_HOOK__ = false
+}
+
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+
