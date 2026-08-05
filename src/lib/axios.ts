c717010a4576db59acd8ff46487abc5aa92b7962
Jose Luis Barbosa <kirbi0w0@gmail.com>
2026-08-05 18:38:46 -0500

    change enviroment variables

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,27 +2,27 @@ import { useAuthStore } from "@/stores/auth";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
-    withXSRFToken: true,
-    headers: {
-        'Accept': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-    }
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    'Accept': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  }
 });
 
 // Interceptor para añadir el Bearer Token automáticamente
 axiosInstance.interceptors.request.use((config) => {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore();
 
-    if (authStore.isAuthenticated) {
-        config.headers.Authorization = authStore.getFullToken;
-    }
+  if (authStore.isAuthenticated) {
+    config.headers.Authorization = authStore.getFullToken;
+  }
 
-    return config;
+  return config;
 
 }, (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 export default axiosInstance;
 
